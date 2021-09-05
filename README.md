19165180f060cfeba5bbad8654bb66e78aebb026
jennimay <mayumi.jenni@gmail.com>
2021-09-05 17:39:34 -0300

    projeto para o checkpoint de front end II

---
+++ README.md
@@ -0,0 +1,32 @@
+
+## Checkpoint I - Front end II
+#
+Projeto realizado para o checkpoint I de front end II, do curso Certified Tech Developer.
+##
+Código base: https://github.com/jennimay/CheckpointI-FrontEndII
+#
+    Objetivo:
+
+	Olá Houser, tudo bem com você? Esta atividade tem como objetivo servir como primeiro instrumento avaliativo da disciplina. Fique atento às instruções e boa avaliação. O entregável será uma página em HTML5, CSS3 e Javascript seguindo os conteúdos ministrados até a aula 08. 
+
+Instruções e requisitos do entregável:
+	
+	
+O tema da aplicação é de sua preferência. Pode ser sobre carros, animes, cidades ou outro tema. A aplicação deve ser responsiva.
+
+
+A aplicação consiste em um formulário que ao ser preenchido cria cards com as informações preenchidas.
+
+
+FORMULÁRIO: 
+O formulário deve ter pelo menos 4 campos: Título, Descrição e URL da imagem e um botão de submit.
+Após o preenchimento, ao clicar no botão de submit, as informações devem ser adicionadas a um novo card na página.
+Exemplo de formulário:
+
+
+CARDS:
+Os cards devem ter uma imagem e constar as informações previamente preenchidas no formulário. O comportamento se dará de forma que as informações preenchidas sejam exibidas nesta seção de cards.
+
+
+Conclusão
+Parabéns! Você acaba de finalizar o primeiro checkpoint de frontend!
---
+++ assets/js/app.js
@@ -0,0 +1,58 @@
+/* botão cadastrar - form */
+let btnCadastrar = document.getElementById("btnCadastrar");
+
+//função para obter os valores inseridos pelo usuário e criar o card com essas informações, através do click no botão cadastrar dentro do formulário
+btnCadastrar.addEventListener("click", function(event){
+    //previnindo evento padrão do botão tipo submit (reload da página)
+    event.preventDefault();
+    
+    //pegando os valores adicionados nos inputs pelo usuário
+    let titulo = document.getElementById("titulo").value;
+    let urlImg = document.getElementById("urlImg").value;
+    let receita = document.getElementById("receita").value;
+    let ingredientes = document.getElementById("ingredientes").value;
+
+    //imprimindo na página - criando o card com os valores que o usuário adicionou no formulário
+        //Selecionando o local do html que vai ser criado o card
+    let card = document.querySelector(".containerCard");
+    card.innerHTML += `<article class="card"">
+                            <div class="divImg">
+                                <img src="${urlImg}" alt="Imagem ${titulo}">
+                            </div>
+                            <div class="informacoes">
+                                <h2 class="tituloCard">${titulo}</h2>
+                                <article class="cardIngredientes" id="cardIngredientes">
+                                <h3>Ingredientes</h3>
+                                <p class="cardIngredites__paragrafo">${ingredientes}</p>
+                                </article>
+                                <article class="cardReceita" id="cardReceita">
+                                    <h3>Modo de preparo</h3>
+                                    <p class="cardReceita__paragrafo">${receita}</p>
+                                </article>
+                            </div>
+                        </article>`;
+
+    /* display none no form */
+    document.querySelector(".sectionForm").style.display = "none";
+
+    /* limpando os campos */
+    document.getElementById("titulo").value = "";
+    document.getElementById("urlImg").value = "";
+    document.getElementById("receita").value = "";
+    document.getElementById("ingredientes").value = "";
+
+});
+
+/* botão para cadastrar mais cards */
+let cadastrarReceita = document.getElementById("cadastrarReceita");
+//função abrir formulário através do botão "cadastrar receita" na nav
+cadastrarReceita.addEventListener("click", () => {
+    document.querySelector(".sectionForm").style.display = "block";
+});
+/* botão cancelar */
+let btnReset = document.getElementById("btnReset");
+    /* função fechar formulário através do botão "cancelar" dentro do formulário*/
+btnReset.addEventListener("click", () => {
+        document.querySelector(".sectionForm").style.display = "none";
+});
+
