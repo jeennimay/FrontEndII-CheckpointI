8ae646fffca8c571c90efe193226ec9b92360d49
jennimay <mayumi.jenni@gmail.com>
2021-09-08 19:46:33 -0300

    img cooking.png

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,6 +17,17 @@ btnCadastrar.addEventListener("click", function(event){
     let receita = document.getElementById("receita").value;
     let ingredientes = document.getElementById("ingredientes").value;
 
+    //validação dos dados
+    if (urlImg == "" || receita == "" || ingredientes == "") {
+        urlImg = "./assets/img/cooking.png";
+        receita = "Hamburger tempor beef pancetta quis. Duis irure deserunt, adipisicing in cow laborum veniam tongue laboris bacon shank filet mignon sed picanha. Ipsum ball tip pancetta sint in fatback. Magna frankfurter culpa enim cupim boudin strip steak bacon consequat fugiat jowl sint. Burgdoggen shoulder beef ribs, occaecat landjaeger bacon bresaola laborum ut beef est. Ground round dolore sint, consectetur landjaeger pork belly laborum tongue bacon porchetta turkey buffalo kevin biltong proident.";
+        ingredientes = "I love cheese, especially stinking bishop cottage cheese. Squirty cheese stinking bishop pecorino melted cheese cut the cheese red leicester ricotta red leicester. Manchego macaroni cheese cheesecake bavarian bergkase macaroni cheese fondue danish fontina caerphilly. Camembert de normandie camembert de normandie port-salut cut the cheese cheeseburger the big cheese chalk and cheese lancashire. Airedale paneer mozzarella roquefort cream cheese airedale queso the big cheese. Cheddar ricotta hard cheese cow fromage cheddar feta mozzarella. Fondue chalk and cheese.";
+    }else{
+        urlImg = document.getElementById("urlImg").value;
+        receita = document.getElementById("receita").value;
+        ingredientes = document.getElementById("ingredientes").value;
+    }
+
     //imprimindo na página - criando o card com os valores que o usuário adicionou no formulário
         //Selecionando o local do html que vai ser criado o card
     let card = document.querySelector(".containerCard");
@@ -27,8 +38,8 @@ btnCadastrar.addEventListener("click", function(event){
                             <div class="informacoes">
                                 <h2 class="tituloCard">${titulo}</h2>
                                 <section class="receita">
-                                    <a id="ingrediente">Ingredientes</a>
-                                    <a id="modoDePreparo">Modo de preparo</a>
+                                    <h3 id="ingrediente">Ingredientes</h3>
+                                    <h3 id="modoDePreparo">Modo de preparo</h3>
                                 </section>
                                 <article class="cardIngredientes" id="cardIngredientes">
                                     <p class="cardIngredites__paragrafo">${ingredientes}</p>
@@ -48,6 +59,9 @@ btnCadastrar.addEventListener("click", function(event){
     document.getElementById("receita").value = "";
     document.getElementById("ingredientes").value = "";
 
+    
+
+    ingredienteReceita();
 });
 
 /* botão para cadastrar mais cards */
@@ -62,4 +76,3 @@ let btnReset = document.getElementById("btnReset");
 btnReset.addEventListener("click", () => {
         document.getElementById("formCadastro").style.display = "none";
 });
-
